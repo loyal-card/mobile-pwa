0f608be9ff1a92c62bee14b37d7ec8923f1b152f
GuoHua Li <ligu@192-168-1-168.tpgi.com.au>
2020-03-16 22:25:06 +1100

    use imbeded login

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import createAuth0Client from '@auth0/auth0-spa-js';
+// import createAuth0Client from '@auth0/auth0-spa-js';
+import Auth0Lock from "auth0-lock";
 
 function App() {
-  let auth0;
+  var lock = new Auth0Lock(
+    '4sh93YHeF9hWItsRv9qHQH31tudFKDpY',
+    'dev-l7c1wrq9.au.auth0.com',{
+      auth: {
+        audience: 'https://localhost:5000',
+      }
+    }
+  );
+
+  lock.on("authenticated", function(authResult) {
+    // Use the token in authResult to getUserInfo() and save it if necessary
+    console.log(authResult.accessToken)
+    window.sessionStorage.setItem('accessToken', authResult.accessToken)
+    this.getUserInfo(authResult.accessToken, function(error, profile) {
+      if (error) {
+        // Handle error
+        return;
+      }
+
+      console.log(profile)
+
+    });
+  });
+
+
   const redirect = async() => {
     // either with async/await
-    auth0 = await createAuth0Client({
-      domain: 'dev-l7c1wrq9.au.auth0.com',
-      audience: 'https://localhost:5000',
-      client_id: '4sh93YHeF9hWItsRv9qHQH31tudFKDpY'
-    });
-    await auth0.loginWithPopup();
+    // auth0 = await createAuth0Client({
+    //   domain: 'dev-l7c1wrq9.au.auth0.com',
+    //   audience: 'https://localhost:5000',
+    //   client_id: '4sh93YHeF9hWItsRv9qHQH31tudFKDpY'
+    // });
+    // await auth0.loginWithPopup();
 
-    const user = await auth0.getUser();
-    console.log(user);
+    // const user = await auth0.getUser();
+    // console.log(user);
+    lock.show();
   }
 
   const logout = async() => {
     // either with async/await
 
-    auth0.logout({
-      returnTo: 'https://localhost:3001/'
-    });
-
+    lock.logout();
   }
   
   const getCode = async() => {
-    const accessToken = await auth0.getTokenSilently();
+    //const accessToken = await auth0.getTokenSilently();
+    const accessToken = window.sessionStorage.getItem('accessToken')
     const result = await fetch('http://localhost:5000/api/get-code', {
       method: 'GET',
       headers: {
