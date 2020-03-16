c328f64e0cd716a1c5f0c945a48b3b295c7b0df3
GuoHua Li <ligu@192-168-1-168.tpgi.com.au>
2020-03-16 12:38:03 +1100

    Co-authored-by: Coco Li <coco.li7777@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 function App() {
+  let auth0;
+  const redirect = async() => {
+    // either with async/await
+    auth0 = await createAuth0Client({
+      domain: 'dev-l7c1wrq9.au.auth0.com',
+      client_id: '4sh93YHeF9hWItsRv9qHQH31tudFKDpY'
+    });
+    await auth0.loginWithPopup();
+
+    const user = await auth0.getUser();
+    console.log(user);
+  }
+
+  const logout = async() => {
+    // either with async/await
+
+    auth0.logout({
+      returnTo: 'https://localhost:3001/'
+    });
+
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +41,8 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={redirect}>Login</button>
+        <button onClick={logout}>Log out</button>
       </header>
     </div>
   );
