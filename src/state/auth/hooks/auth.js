a2855259fb147463b2d5b0310879b8860ee5b419
Coco <coco.li7777@gmail.com>
2020-04-12 16:28:21 +1000

    auth hook

--- src/state/auth/hooks/auth.js
+++ src/state/auth/hooks/auth.js
@@ -1,9 +1,36 @@
-import { useState } from 'react';
 import { useStateValue } from '../../index';
 import { login } from '../actions';
+import Auth0Lock from 'auth0-lock';
 
 const useAuth = () => {
   const [{ auth }, dispatch] = useStateValue();
+
+  var lock = new Auth0Lock(
+    '4sh93YHeF9hWItsRv9qHQH31tudFKDpY',
+    'dev-l7c1wrq9.au.auth0.com',
+    {
+      auth: {
+        audience: 'https://localhost:5000',
+      },
+    }
+  );
+
+  lock.on('authenticated', function (authResult) {
+    // Use the token in authResult to getUserInfo() and save it if necessary
+    console.log(JSON.stringify(authResult));
+    dispatch(login);
+  });
+  const signIn = () => {
+    lock.show();
+  };
+
+  const siginOut = async () => {
+    // either with async/await
+
+    lock.logout();
+  };
+
+  return [auth, signIn, siginOut];
 };
 
 export default useAuth;
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import useAuth from '../../state/auth/hooks/auth';
 
 const Login = (props) => {
+  const { from } = props.location.state || { from: { pathname: '/home' } };
+
+  const [auth, signIn] = useAuth();
+  useEffect(() => {
+    console.log(JSON.stringify(auth));
+    if (auth.authenticated) {
+      debugger;
+      props.history.push(from);
+    }
+  }, [auth]);
+
   return (
     <div>
-      <button>Login</button>
+      <button onClick={() => signIn()}>Login</button>
       <button>Register</button>
     </div>
   );
