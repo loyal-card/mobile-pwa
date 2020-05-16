783210aaaa1dfca75b4bd9c06302e789ffa25cc6
Alex Li <guohua_lee@163.com>
2020-05-17 09:57:34 +1000

    Updates

---
+++ precache-manifest.a02acb70d76dab58fdb1eadba7ff5687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29526393866a5a56f29ebbb068083d9",
+    "url": "/loyal-card.github.io/mobile-pwa/index.html"
+  },
+  {
+    "revision": "da03235d8edddb2b0dc6",
+    "url": "/loyal-card.github.io/mobile-pwa/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3d8874efbdfe9c44ed7e",
+    "url": "/loyal-card.github.io/mobile-pwa/static/js/2.bdbcfe4c.chunk.js"
+  },
+  {
+    "revision": "333f62e53a3409dbf5e7809c1f008706",
+    "url": "/loyal-card.github.io/mobile-pwa/static/js/2.bdbcfe4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da03235d8edddb2b0dc6",
+    "url": "/loyal-card.github.io/mobile-pwa/static/js/main.15cdbf16.chunk.js"
+  },
+  {
+    "revision": "cec9d5ff2b8fca8a7106",
+    "url": "/loyal-card.github.io/mobile-pwa/static/js/runtime-main.bad60582.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/loyal-card.github.io/mobile-pwa/precache-manifest.a02acb70d76dab58fdb1eadba7ff5687.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/loyal-card.github.io/mobile-pwa/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
